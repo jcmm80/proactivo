c4fbafbe43a76f707929cf06285ac1fc9fec1939
jcmm80 <jhomartic321@gmail.com>
2023-06-17 13:26:06 -0500

    Septimo commit
    Cambios jhonatan vista coordinador

--- web/resources/js/menu.js
+++ web/resources/js/menu.js
@@ -5,12 +5,15 @@ const navMenu = document.getElementById('nav_menu'),
 if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show_menu');
+
     });
+
 }
 
 if(navClose){
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show_menu');
+
     });
 }
 
@@ -18,6 +21,7 @@ const navLinkV = document.querySelectorAll('.nav-vertical-link');
 const navLinkH = document.querySelectorAll('.nav-horizontal-link');
 const linkAction = () =>{
     navMenu.classList.remove('show-menu');
+
 };
 navLinkV.forEach(n => n.addEventListener('click', linkAction));
 navLinkH.forEach(n => n.addEventListener('click', linkAction));
